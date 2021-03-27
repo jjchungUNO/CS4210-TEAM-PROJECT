03e7497c3626a9aff36673f250b68b58c19f19d9
Joseph Adogeri <jadogeri@uno.edu>
2021-03-26 20:02:00 -0500

    moved all files in csci4210 package

---
+++ TeamManagement/src/CSCI4210/OutOfRangeException.java
@@ -0,0 +1,21 @@
+package CSCI4210;
+
+
+import java.lang.IllegalArgumentException;
+/**
+ * @author JOSEPH ADOGERI
+ *
+ */
+public class OutOfRangeException extends IllegalArgumentException {
+	
+	public OutOfRangeException(String message) {
+		//pass in message
+		super(message);
+	}
+	
+	public OutOfRangeException() {
+		//default message
+		super("value of double must be between 1 and 0");
+	}
+
+}
